4da25224916b20c671cefe7c3c2ffc332cc142bf
KosukeIba <kosuke.iba@tcs.com>
2023-11-23 18:44:34 +0900

    feat: add button to fetch photos from db

--- src/routes/protected/dashboard/+page.svelte
+++ src/routes/protected/dashboard/+page.svelte
@@ -21,6 +21,8 @@
     
       <div class="thread">
         <div>
+          <button class="thread--commonButton">enjoy photos!</button>
+        <div>
           <ul>
             {#each photos as photo}
               <div class="thread--postContainer">
@@ -48,6 +50,14 @@
 
 
 <style>
+  button {
+    padding: 0;
+    font-family: inherit;
+    appearance: none;
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+}
   li {
     list-style: none;
   }
@@ -68,6 +78,32 @@
     border-left: slategray solid;
     border-width: 1px;
   }
+  .thread--commonButton {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80px;
+    aspect-ratio: 1;
+    color: rgb(66, 66, 66);
+    background-color: #efefef;
+    border-radius: 50%;
+    border-color: rgb(18, 51, 40);
+    box-shadow: -4px -4px 8px #fff, 4px 4px 8px rgba(0, 0, 0, 0.24);
+  }
+  @media (any-hover: hover) {
+  .thread--commonButton {
+    transition: box-shadow 0.2s;
+  }
+
+  .thread--commonButton:hover {
+    box-shadow: -2px -2px 4px #fff, 2px 2px 4px rgb(0 0 0 / 24%);
+  }
+
+  .thread--commonButton:active {
+    background-color: #ebebeb;
+    box-shadow: inset 4px 4px 8px rgb(0 0 0 / 16%);
+  }
+}
   .thread--postContainer {
     padding: 20px;
   }
